5127bb0704c6f10edb101144882dbd04c5501072
Isabella <isabella.cabrera@aluno.senai.br>
2026-02-06 11:51:51 -0300

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Image} from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require('./assets/login.png')} 
+      style={{ width: 140, height: 140, borderRadius: 300, marginTop: 20 }}/>
+      <Text style={styles.text}>Join Us!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#758fad',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
 });
+
